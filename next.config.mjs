430db64a710d43434190f3cf71398934794aeb4e
Koha <omobolathejoshua@gmail.com>
2024-09-08 17:56:55 +0100

    feat: init dscvr-ai

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const cspHeader = `
+    default-src 'self' *.trycloudflare.com;
+    img-src 'self' *.trycloudflare.com;
+    connect-src 'self' *.trycloudflare.com
+    script-src 'self' *.trycloudflare.com;
+    style-src 'self' *.trycloudflare.com;
+    font-src 'self' *;
+    object-src 'none' *;
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+
+const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'Content-Security-Policy',
+                value: cspHeader.replace(/\n/g, ''),
+              },
+            ],
+          },
+        ]
+      },
+};
 
 export default nextConfig;
